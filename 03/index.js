eb8bef6888cb7459960af2701c4248be6a0b1b94
Daniel bruno <danielbrunodsn@gmail.com>
2023-07-26 16:32:52 -0300

    resolvido

---
+++ 03/index.js
@@ -0,0 +1,7 @@
+var precoTenis = 129.99
+var valorDisponivel = 80.00
+var desconto = precoTenis - valorDisponivel
+var porcentagemNecessaria = (desconto/precoTenis) * 100
+
+console.log("Porcentagem necessária é:", porcentagemNecessaria.toFixed(2))
+
