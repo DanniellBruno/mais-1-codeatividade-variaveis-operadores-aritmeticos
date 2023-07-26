3a9dd60da4d3202427dd390d0a1fc4b063221692
Daniel bruno <danielbrunodsn@gmail.com>
2023-07-26 16:34:22 -0300

    resolvido

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,7 @@
 const raio = 1
+const pi = 3.14
+const comprimentoCirculo = 2 * pi * raio
+const areaCirculo =  pi * (raio ** 2)
+
+console.log("Comprimento do círculo é:", comprimentoCirculo.toFixed(2))
+console.log("Area do Círculo é:", areaCirculo.toFixed(2))
