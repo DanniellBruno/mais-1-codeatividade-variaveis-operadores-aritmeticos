544533e8f6eef0c8a00b17b16739fa0923a1ad70
Daniel bruno <danielbrunodsn@gmail.com>
2023-07-26 16:36:01 -0300

    resolvido

---
+++ 10/index.js
@@ -0,0 +1,7 @@
+var raioCilindro = 3
+var alturacilindro = 1
+var pi = 3.14
+
+var areaTotal = 2 * pi * raioCilindro * (raioCilindro + alturacilindro)
+
+console.log("A area total do cilindro é: ", areaTotal.toFixed(2))
