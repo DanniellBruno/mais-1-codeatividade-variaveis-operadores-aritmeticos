6c905917bb6fa40c9c38ff00504e952d87d6d5d4
Daniel bruno <danielbrunodsn@gmail.com>
2023-07-26 16:35:36 -0300

    resolvido

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,7 @@
-const diâmetro = 3
+const diametro = 3
+const pi = 3.14
+const raio = diametro / 2
+
+const volume = (4 /3) * pi * Math.pow(raio, 3)
+
+console.log("O volume de uma esfera de raio 3 é: ", volume.toFixed(2))
