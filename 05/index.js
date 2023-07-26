6180a34087bef5549ffde6014163f849a8149a49
Daniel bruno <danielbrunodsn@gmail.com>
2023-07-26 16:33:48 -0300

    resolvido

---
+++ 05/index.js
@@ -0,0 +1,6 @@
+var distanciaPecorrida = 500 
+var tempo = 10
+var velocidade = distanciaPecorrida/tempo
+var distanciaEmKm = velocidade * 3.6
+
+console.log("Distancia pcorrida em KM é:", distanciaEmKm)
